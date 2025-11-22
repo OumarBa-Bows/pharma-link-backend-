043c7261fb03855b65fb67c4489a40a18c427d88
hamel-dev <bhamil179@gmail.com>
2025-11-22 11:15:43 +0000

    synchronize : false

--- src/entities/Pharmacy.entity.ts
+++ src/entities/Pharmacy.entity.ts
@@ -3,10 +3,6 @@ import { PharmacyState } from '../enums/PharmacyState.enum';
 import { User } from './User.entity';
 import { Zone } from './Zone.entity';
 
-// export enum CustomerType {
-//   PHARMACY = 'PHARMACY',
-//   DEPOT = 'DEPOT'
-// }
 import { PharmacyCustomerType } from "../enums/PharmacyCustomerType";
 import { Customer } from "./Customer.entity";
 import { UUID } from 'crypto';
