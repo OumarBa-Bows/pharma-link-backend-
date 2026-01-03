76a68a9ba1f1158b94489b4e71f09d523f0d353a
hamel-dev <bhamil179@gmail.com>
2026-01-03 12:15:05 +0000

    fix: set synchronize option to false in TypeOrmDataSource configuration

--- src/configs/TypeOrmDataSource.ts
+++ src/configs/TypeOrmDataSource.ts
@@ -11,7 +11,7 @@ export const AppDataSourceConfig: DataSourceOptions = {
   username: process.env.SUPABASE_USER,
   password: process.env.SUPABASE_PASS,
   database: process.env.SUPABASE_DBNAME,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   poolSize: 50,
   extra: {
